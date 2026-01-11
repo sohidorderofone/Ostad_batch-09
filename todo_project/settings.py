1ffda3e295929d4ce270f9f2a879621549e0edf7
Sohid Ullah <sohid.cse@gmail.com>
2026-01-11 13:24:23 +0600

    add ostad-batch-09.onrender.com to allowed host

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     'subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     '.ngrok-free.dev',  # Allow all ngrok-free.dev subdomains
     '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
+    'ostad-batch-09.onrender.com'
 ]
 
 # CSRF trusted origins for ngrok
